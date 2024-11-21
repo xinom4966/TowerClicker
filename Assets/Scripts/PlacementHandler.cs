31c67c453d7accf597e3aea50de763cb9ecbac50
xinom4966 <sd.dessay@gmail.com>
2024-11-21 14:04:26 +0100

    Project Setup

---
+++ Assets/Scripts/PlacementHandler.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum PlacementState
+{
+    Fixed,
+    Valid,
+    Invalid
+}
+
+public class PlacementHandler : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Color _validColor;
+    [SerializeField] private Color _invalidColor;
+    private Color _fixedColor;
+    [HideInInspector] public bool hasValidPlacement;
+    [HideInInspector] public bool isFixed;
+    private int _obstacleNumber;
+    private TowerPlacer _towerPlacer;
+
+    private void Awake()
+    {
+        hasValidPlacement = true;
+        isFixed = true;
+        _obstacleNumber = 0;
+        if (_spriteRenderer != null)
+        {
+            _fixedColor = _spriteRenderer.color;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isFixed) { return; }
+        _obstacleNumber++;
+        SetPlacementState(PlacementState.Invalid);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (isFixed) { return; }
+        _obstacleNumber--;
+        if (_obstacleNumber <= 0)
+        {
+            SetPlacementState(PlacementState.Valid);
+        }
+    }
+
+    public void SetPlacementState(PlacementState p_state)
+    {
+        if (p_state == PlacementState.Fixed)
+        {
+            isFixed = true;
+            hasValidPlacement = true;
+        }
+        else if (p_state == PlacementState.Valid)
+        {
+            hasValidPlacement = true;
+        }
+        else
+        {
+            hasValidPlacement = false;
+        }
+        SetColor(p_state);
+    }
+
+    private void SetColor(PlacementState p_state)
+    {
+        switch (p_state)
+        {
+            case PlacementState.Fixed:
+                if (_spriteRenderer != null)
+                    _spriteRenderer.color = _fixedColor;
+                break;
+            case PlacementState.Valid:
+                if (_spriteRenderer != null)
+                    _spriteRenderer.color = _validColor;
+                break;
+            case PlacementState.Invalid:
+                if (_spriteRenderer != null)
+                    _spriteRenderer.color = _invalidColor;
+                break;
+            default:
+                if (_spriteRenderer != null)
+                    _spriteRenderer.color = _fixedColor;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/TowerPlacer.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TowerPlacer : MonoBehaviour
+{
+    protected GameObject _towerPrefab;
+    protected GameObject _toBuild;
+    protected Camera _camera;
+    protected Vector3 _mousePos;
+
+    private void Awake()
+    {
+        _towerPrefab = null;
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (_towerPrefab != null)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Destroy(_toBuild);
+                _toBuild = null;
+                _towerPrefab = null;
+                return;
+            }
+
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                _toBuild.SetActive(false);
+            }
+            else
+            {
+                _toBuild.SetActive(true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _toBuild.transform.Rotate(Vector3.forward, 90);
+            }
+
+            _mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+            _toBuild.transform.position = new Vector3(_mousePos.x, _mousePos.y, 0);
+            if (Input.GetMouseButtonDown(0))
+            {
+                PlacementHandler handler = _toBuild.GetComponent<PlacementHandler>();
+                if (handler.hasValidPlacement)
+                {
+                    handler.SetPlacementState(PlacementState.Fixed);
+
+                    _towerPrefab = null;
+                    _toBuild = null;
+                }
+            }
+        }
+    }
+
+    public void SetTowerPrefab(GameObject p_prefab)
+    {
+        _towerPrefab = p_prefab;
+        PrepareTower();
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    protected virtual void PrepareTower()
+    {
+        if (_toBuild)
+        {
+            Destroy(_toBuild);
+        }
+        _toBuild = Instantiate(_towerPrefab);
+        _toBuild.SetActive(false);
+
+        PlacementHandler handler = _toBuild.GetComponent<PlacementHandler>();
+        handler.isFixed = false;
+        handler.SetPlacementState(PlacementState.Valid);
+    }
+}

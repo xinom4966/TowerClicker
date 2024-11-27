a98b78637243d69e1c545cc2076bb8788945776f
xinom4966 <sd.dessay@gmail.com>
2024-11-27 10:44:08 +0100

    Finished shop system

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,22 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
     [SerializeField] private Player _player;
     [SerializeField] private TowerPlacer _placer;
+    [SerializeField] private GameObject _shopGO;
     private int _playerDebt = 0;
 
     public void BeginTransaction(GameObject towerBoughtGO)
     {
-        Tower towerBought = towerBoughtGO.GetComponent<Tower>();
+        Tower towerBought = towerBoughtGO.GetComponentInChildren<Tower>(true);
+        if (towerBought == null)
+        {
+            return;
+        }
         if (!_player.CheckPrice(towerBought))
         {
             return;
         }
-        _placer.SetTowerPrefab(towerBought.gameObject);
+        _placer.SetTowerPrefab(towerBought.transform.parent.gameObject);
         _playerDebt = towerBought.GetCost();
+        _shopGO.SetActive(false);
     }
 
     public void EndTransaction()

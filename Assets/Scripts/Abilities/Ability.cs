f68e5f2aa9bea0468d6208e04f465fe29f50b78a
xinom4966 <sd.dessay@gmail.com>
2025-05-05 17:26:41 +0200

    Started the abilities
    My brain's not working I cannot think of an optimal way to wipe all ennemies on screen.

---
+++ Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ability : MonoBehaviour
+{
+    [SerializeField] private float _cooldown;
+    [SerializeField] private Image _image;
+    private bool _usable = true;
+    private float _timer = 0.0f;
+
+    protected virtual void Update()
+    {
+        if (!_usable)
+        {
+            _timer += Time.deltaTime;
+            CoolDownFeedBack(_timer/_cooldown);
+            if (_timer >= _cooldown)
+            {
+                _usable = true;
+                _timer = 0.0f;
+            }
+        }
+    }
+
+    public virtual void Execute()
+    {
+        if (!_usable)
+        {
+            return;
+        }
+        _usable = false;
+    }
+
+    protected void CoolDownFeedBack(float ratio)
+    {
+        _image.fillAmount = ratio;
+    }
+
+    protected void HoverFeedBack()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Abilities/WipeAbility.cs
@@ -0,0 +1,12 @@
+public class WipeAbility : Ability
+{
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Execute()
+    {
+        base.Execute();
+    }
+}
